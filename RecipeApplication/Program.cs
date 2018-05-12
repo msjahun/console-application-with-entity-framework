a7659daa01f0c53880625f7041b24df2c591c408
msjahun <msjahun@live.com>
2018-05-12 14:30:02 +0200

    created some highlevel CRUD methods

--- RecipeApplication/Program.cs
+++ RecipeApplication/Program.cs
@@ -6,10 +6,122 @@ using System.Threading.Tasks;
 
 namespace RecipeApplication
 {
+    
     class Program
     {
+      
+
+
+
         static void Main(string[] args)
         {
+         void createRecord(String newRecName)
+            {
+                using (testDbEntities context = new testDbEntities())
+                {
+                    Recipe recipe = new Recipe
+                    {
+                      Name = newRecName
+                    };
+
+                   context.Recipes.Add(recipe);
+                    context.SaveChanges();
+                }
+           }
+
+                void readRecord(int recordId)
+            {
+                using (testDbEntities context = new testDbEntities())
+                {
+                    Recipe recipe = context.Recipes.FirstOrDefault(r => r.Id == recordId);
+                   if( !recipe.Equals(null))
+                    Console.WriteLine(recipe.Name);
+                }
+            }
+
+                void updateRecord(String recordName, int recordId)
+                {
+                    using (testDbEntities context = new testDbEntities())
+                    {
+                        Recipe recipe = context.Recipes.FirstOrDefault(r => r.Id == recordId);
+                    if (!recipe.Equals(null)) { 
+                        recipe.Name = recordName;
+                        context.SaveChanges();
+                    }
+
+                }
+                }
+
+                    void deleteRecord(int recordId)
+                    {
+                        using (testDbEntities context = new testDbEntities())
+                        {
+
+                    Recipe recipe = context.Recipes.FirstOrDefault(r => r.Id == recordId);
+                    if (!recipe.Equals(null))
+                    {
+                        context.Recipes.Remove(recipe);
+                        context.SaveChanges();
+                    }
+                        }
+                    }
+
+
+
+            Console.WriteLine("Program initiated");
+
+            createRecord("Chicken Sautee");
+            createRecord("Beef Sautee");
+            createRecord("Pinnaple wJuice");
+            createRecord("Chicken werSausdftee");
+            createRecord("Beef Sawerutesdfe");
+            createRecord("Pinnaple sdfJuice");
+            createRecord("Chicken Ssdfautee");
+            createRecord("Beewref Sautee");
+            createRecord("Pinnweraple Juice");
+
+            readRecord(2);
+            readRecord(3);
+            readRecord(4);
+            readRecord(5);
+            readRecord(6);
+            readRecord(7);
+            readRecord(8);
+
+            updateRecord("Musa sause", 4);
+            updateRecord("Musa sautee", 5);
+
+
+            readRecord(2);
+            readRecord(3);
+            readRecord(4);
+            readRecord(5);
+            readRecord(6);
+            readRecord(7);
+            readRecord(8);
+
+
+            deleteRecord(3);
+            deleteRecord(4);
+            deleteRecord(5);
+
+
+
+
+            readRecord(2);
+            readRecord(3);
+            readRecord(4);
+            readRecord(5);
+            readRecord(6);
+            readRecord(7);
+            readRecord(8);
+
+            Console.ReadLine();
+
+
         }
+
     }
+
+   
 }
